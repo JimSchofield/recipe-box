1fe227cb15e7cbfe90b45957a9a9848ddae27897
Matt Hagner <web@matthagner.com>
2020-06-19 17:26:17 -0500

    Add modal to App component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
     <router-view />
+    <transition name="fade">
+      <div id="modal" v-if="modalIsOpen">
+        <p>Hey dere</p>
+        <button @click="modalClose">Close Me Please</button>
+      </div>
+    </transition>
   </div>
 </template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+  name: "App",
+  computed: {
+    ...mapState({ modalIsOpen: state => state.modalModule.isOpen })
+  },
+  methods: {
+    ...mapActions(["modalClose"])
+  }
+};
+</script>
 
 <style lang="scss">
 body {
@@ -14,5 +34,24 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+
+#modal {
+  background: lightgray;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-height: 300px;
+  width: 100%;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 300ms;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
